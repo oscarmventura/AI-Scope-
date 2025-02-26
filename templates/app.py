05e1e62edc8200beaf0149539e1dac49d960a6b5
Your Name <oscarmventura@icloud.com>
2025-02-25 21:05:34 -0600

    initial commit

---
+++ templates/app.py
@@ -0,0 +1,7 @@
+#/* Copyright (c) 2024 Oscar Ventura. All Rights Reserved.
+# *
+#  This software and its source code are the exclusive property of Oscar M Ventura.
+#  Unauthorized copying, distribution, modification, or use of this software,
+# in whole or in part, without express written permission from the author is strictly prohibited.
+# For licensing inquiries, please contact oscarmventura@icloud.com
+# /
